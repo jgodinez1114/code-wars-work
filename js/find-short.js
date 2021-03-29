ded98a0bf685ef31387b17602015deeea731722d
jgodinez <jgodinez1114@gmail.com>
2021-03-29 18:50:10 -0500

    completed alternate solution

--- js/find-short.js
+++ js/find-short.js
@@ -1,16 +1,23 @@
 // given a string of words, return the lengfth ofg the shortest word(s)
 
+// function findShortest(s) {
+//     // split the string of strings at (" ")
+//     let wordsArr = s.split(" ");
+//     let shortest = wordsArr[0].length;
+//     // traverse the array
+//     for (a of wordsArr) {
+//         if (a.length < shortest) {
+//             shortest = a.length;
+//         }
+//     }
+//     return shortest;
+// }
+
 function findShortest(s) {
-    // split the string of strings at (" ")
-    let wordsArr = s.split(" ");
-    let shortest = wordsArr[0].length;
-    // traverse the array
-    for (a of wordsArr) {
-        if (a.length < shortest) {
-            shortest = a.length;
-        }
-    }
-    return shortest;
+    return s
+        .split(' ')
+        .map(a => a.length)
+        .reduce((a, b) => Math.min(a, b))
 }
 
 // sample tests
@@ -20,3 +27,7 @@ function findShortest(s) {
 // });
 
 console.log(findShortest("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShortest("lorem ipsum this is a super test string as a great testssss."));
+
+// alternate solution
+
