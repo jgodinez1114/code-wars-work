b5ee6aed164c0d7d741d1ffc59a9fb2d7913dff5
jgodinez <jgodinez1114@gmail.com>
2021-05-22 09:48:47 -0500

    completed alternate solution

--- js/counting-duplicates.js
+++ js/counting-duplicates.js
@@ -1,26 +1,32 @@
 
 
 function duplicateCount(text) {
-    text = text.toLowerCase()
-    let letters = text.split('')
+    let letters = text.toLowerCase().split("")
     let counter = {}
     let duplicates = 0
     // traverse letters in text
     for (a in letters) {
+        // check if this letter exists in counter object
         if (!counter[letters[a]]) {
+            // if not, initialize counter for this element at 0
             counter[letters[a]] = 0
         }
-
+        // increment value for this element
         ++counter[letters[a]]
     }
     // traverse the keys in counter
     for (i in counter) {
-        if (couter[i] > 1) {
+        if (counter[i] > 1) {
             duplicates++
         }
     }
     return duplicates;
 }   // end duplicateCount()
+
+// alternate solution
+function betterDuplicateCount(text) {
+    return (text.split('').sort().join('').match(/([^])\1+/gi) || []).length;
+}
 
 
 describe("Tests", () => {
