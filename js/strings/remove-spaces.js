56588fb9c9c93a47af97f466181dc4da3e4cc786
jgodinez <jgodinez1114@gmail.com>
2021-06-01 11:51:55 -0500

    completed alternate solution with regex

--- js/strings/remove-spaces.js
+++ js/strings/remove-spaces.js
@@ -4,6 +4,16 @@ function noSpace(x) {
     return x.split(' ').join('')
 }
 
+// alternate solution
+function betterNoSpace(x) {
+    /*
+     \s means "any white space character, including space, tab, form feed or line feed"
+     g is a flag meaning "global search"
+     so /\s/g means "look for every white space char."
+     */
+    return x.replace(/\s/g, '')
+}
+
 console.log(noSpace('kdjf kdjf kdkd    d d           df3'));
 console.log(noSpace('kdjf kdjf kdkdd ddf3'));
 console.log(noSpace('    d d           df3'));
